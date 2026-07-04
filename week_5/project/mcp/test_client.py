3f544f4299d441ae2fafdea1b76bb2f379b6aa5c
Prakamya Siddh Balot <mt1251387@maths.iitd.ac.in>
2026-07-04 12:04:16 +0530

    Add configuration loading for GitHub server
    Load configuration from 'config.json' and set up environment variables for the GitHub server.

--- week_5/project/mcp/test_client.py
+++ week_5/project/mcp/test_client.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+import os
 
 from mcp import (
     ClientSession,
@@ -8,3 +10,44 @@ from mcp import (
 from mcp.client.stdio import (
     stdio_client
 )
+with open(
+    "config.json",
+    "r",
+    encoding="utf-8"
+) as f:
+
+    config = json.load(f)
+
+github = config["mcpServers"]["github"]
+
+env = {}
+
+for key, value in github["env"].items():
+
+    if (
+        value.startswith("${")
+        and value.endswith("}")
+    ):
+
+        variable = value[2:-1]
+
+        env[key] = os.environ.get(
+            variable,
+            ""
+        )
+
+    else:
+
+        env[key] = value
+
+server = StdioServerParameters(
+
+    command="npx.cmd",
+
+    args=github["args"],
+
+    env=env
+
+)
+
+print(server)
